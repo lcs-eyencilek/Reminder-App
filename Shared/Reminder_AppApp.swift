a9bdcac6568a929efb5cd5fe03abc5ccef7ba616
lcs-eyencilek <90844648+lcs-eyencilek@users.noreply.github.com>
2022-01-24 22:29:12 -0500

    Initial Commit

---
+++ Shared/Reminder_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminder_AppApp.swift
+//  Shared
+//
+//  Created by Efe Yencilek on 2022-01-24.
+//
+
+import SwiftUI
+
+@main
+struct Reminder_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
