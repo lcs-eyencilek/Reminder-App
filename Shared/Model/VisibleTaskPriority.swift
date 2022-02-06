66a8038c52863658a12ee02ceb90cbe3f6572778
lcs-eyencilek <90844648+lcs-eyencilek@users.noreply.github.com>
2022-02-06 18:44:05 -0500

    Added a picker to let the user decide what tasks should show based on their priority. TODO: Still need to actually filter the tasks.

---
+++ Shared/Model/VisibleTaskPriority.swift
@@ -0,0 +1,15 @@
+//
+//  VisibleTaskPriority.swift
+//  Reminder App
+//
+//  Created by Efe Yencilek on 2022-02-06.
+//
+
+import Foundation
+
+enum VisibleTaskPriority: String {
+    case high = "High"
+    case medium = "Medium"
+    case low = "Low"
+    case all = "All"
+}
--- Shared/Views/Main Views/ContentView.swift
+++ Shared/Views/Main Views/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
     // Whether to show completed tasks or not
     @State var showingCompletedTasks = true
     
+    // What priority of tasks to show
+    @State private var selectedPriorityForVisibleTasks: VisibleTaskPriority = .all
+    
     // Controls the update status of the ContentView
     // listShouldUpdate has to be used somewhere in the body paragraph so that SwiftUI checks this variable to update teh scene
     @State var listShouldUpdate = false
@@ -26,35 +29,53 @@ struct ContentView: View {
         
         // We have this decleration mainly to invoke the screen to refresh
         let _ = print("listShouldUpdate has been toggled. Current value is: \(listShouldUpdate)")
+        VStack {
         
-        List {
-            ForEach(store.tasks) { task in
-                // Dismisses the current task if showImportant is true and the task priority is not equal to high
-                if !showingCompletedTasks {
-                    if !task.completed {
-                        TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
-                    }
-                } else {
-                    TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
-                }
+            HStack {
+                // label for picker
+                Text("Filter by...")
+                    .font(Font.caption.smallCaps())
+                    .foregroundColor(.secondary)
                 
-            }
-            .onDelete(perform: store.deleteItems)
-            .onMove(perform: store.moveItems)
-            // You don't call the function, you just invoke it so no ()
-        }
-        .navigationTitle("Reminders")
-        // Trigger showingAddTask to make the pop-up view appear
-        .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Button("Add") {
-                    showingAddTask.toggle()
+                Picker("Priority", selection: $selectedPriorityForVisibleTasks) {
+                    Text(VisibleTaskPriority.all.rawValue).tag(VisibleTaskPriority.all)
+                    Text(VisibleTaskPriority.low.rawValue).tag(VisibleTaskPriority.low)
+                    Text(VisibleTaskPriority.medium.rawValue).tag(VisibleTaskPriority.medium)
+                    Text(VisibleTaskPriority.high.rawValue).tag(VisibleTaskPriority.high)
                 }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
             }
             
-            ToolbarItem(placement: .bottomBar) {
-                Button(" \(showingCompletedTasks ? "Hide" : "Show") Completed") {
-                    showingCompletedTasks.toggle()
+            List {
+                ForEach(store.tasks) { task in
+                    // Dismisses the current task if showImportant is true and the task priority is not equal to high
+                    if !showingCompletedTasks {
+                        if !task.completed {
+                            TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+                        }
+                    } else {
+                        TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+                    }
+                    
+                }
+                .onDelete(perform: store.deleteItems)
+                .onMove(perform: store.moveItems)
+                // You don't call the function, you just invoke it so no ()
+            }
+            .navigationTitle("Reminders")
+            // Trigger showingAddTask to make the pop-up view appear
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Add") {
+                        showingAddTask.toggle()
+                    }
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                    Button(" \(showingCompletedTasks ? "Hide" : "Show") Completed") {
+                        showingCompletedTasks.toggle()
+                    }
                 }
             }
         }
